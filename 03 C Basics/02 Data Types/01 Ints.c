6d85f9d42a8533d515e04a9ff6f3667a3508a0fa
Natalie Gates <natalielgates05@gmail.com>
2026-01-06 14:26:06 -0600

    changes

--- 03 C Basics/02 Data Types/01 Ints.c
+++ 03 C Basics/02 Data Types/01 Ints.c
@@ -27,12 +27,26 @@ int main() {
     printf("Size of int16_t is %lu bytes\n", sizeof(int16_t));
 
     // long
-    // long integer is typically 8 bytes, but sometimes 4
+    // long integer is typically 8 bytes, but sometimes 4 (such as on Windows)
     long z = 10;
     int64_t z2 = 10;
 
+    // %ld is likely signed long, %lu is likely unsigned long
     printf("Size of long is %lu bytes\n", sizeof(long));
     printf("Size of int64_t is %lu bytes\n", sizeof(int64_t));
+
+    // long long int
+    // long longs are typically 8 bytes (64 bits)
+    long long int a = 10;
+    // could use int64_t
+    printf("The value of a is %lld\n", a);
+
+    // use the word unsigned for unsigned values
+    unsigned int b1 = 10;
+    unsigned int b2 = -10;
+
+    printf("b1 = %u\n", b1);
+    printf("b2 = %u\n", b2);
 
     return 0;
 }
