3f56448b91c00cc3539f4b3b7fc04fbb7a8ae9d6
Natalie Gates <natalielgates05@gmail.com>
2026-01-06 14:36:27 -0600

    changes

---
+++ 03 C Basics/02 Data Types/02 Chars.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    /*
+    Characters or char
+    - single characters
+    - these are ASCII characters
+    - literals are integers or use single quotes like 'A'
+    - chars are 8 bits, or 1 byte
+    */
+   
+    char letter = 'A';
+    printf("letter = %c\n", letter);
+    printf("letter = %d\n", letter);
+    
+    return 0;
+}
