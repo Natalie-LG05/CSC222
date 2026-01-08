eb2f380fa2da9f99d64ad8d5d59498912bdefa68
Natalie Gates <natalielgates05@gmail.com>
2026-01-08 15:45:50 -0600

    changes

---
+++ Lab3/question2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    printf("Enter a negative value at any point to quit.")
+    while (1) {
+        int input;
+        printf("Enter the number of seconds to convert: ");
+        scanf("%d", $input);
+
+        if (input < 0) break;
+
+        printf("%d", input);
+    }
+    
+    return 0;
+}
