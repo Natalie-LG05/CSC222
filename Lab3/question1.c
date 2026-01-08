f3cf17e052312c8632a6110af957dd85232c403a
Natalie Gates <natalielgates05@gmail.com>
2026-01-08 15:40:27 -0600

    changes

--- Lab3/question1.c
+++ Lab3/question1.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    printf("%d\n", argc);
-    
-    for (int i = 0; i < argc; i++ ) {
-        printf("%s ", argv[i]);
-    }
-    printf("\n");
-    return 0;
-    
-    if (argc < 2) {
+int main(int argc, char *argv[]) {    
+    if ((argc - 1) < 2) {
         printf("Must provide at least 2 arguments.\n");
-    } else if (argc > 6) {
+    } else if ((argc - 1) > 6) {
         printf("Must provide at most 6 arguments.\n");
     } else {
-        for (int i = 0; i < argc; i++) {
+        for (int i = 1; i < (argc - 1); i++) {
             if (i % 2 == 1) printf("%c ", argv[i][0]);  // print the 1st character of odd arguments
             else printf("%c ", argv[i][1]);  // print the 2nd character of even arguments
         }
