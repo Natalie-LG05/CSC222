e194ad3d86fb0434f2ee58a7a4fa8354b35bd22f
Natalie Gates <natalielgates05@gmail.com>
2026-01-08 15:36:43 -0600

    changes

--- Lab3/question1.c
+++ Lab3/question1.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(char *argv[], int argc) {
+    for (int i = 0; i < argc; i++ ) {
+        printf("%s ", argv[i]);
+    }
+    printf("\n");
+    
     if (argc < 2) {
         printf("Must provide at least 2 arguments.\n");
     } else if (argc > 6) {
