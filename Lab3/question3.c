abf3b15995ce122557a38e06c39259cdaa6053e7
Natalie Gates <natalielgates05@gmail.com>
2026-01-08 17:24:40 -0600

    changes

--- Lab3/question3.c
+++ Lab3/question3.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    
+    printf("Enter a negative value at any point to quit.\n");
+    while (1) {
+        int num;
+        printf("Enter a non-negative integer: ");
+        scanf("%d", &num);
+
+        // Exit if a negative number is entered
+        if (num < 0) break;
+
+        // Output the given number's binary representation using bitwise operators
+        int num_bits = sizeof(num) * 8;
+        for (int i = (num_bits-1); i >= 0; i--) {
+            printf("%d", (num>>i)&1);
+        }
+        printf("\n");
+    }
     
     return 0;
 }
