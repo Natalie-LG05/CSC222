fab1d689d917f7a292a2197a2a0334abe27ec14d
Natalie Gates <natalielgates05@gmail.com>
2026-01-08 14:01:52 -0600

    changes

--- 03 C Basics/03 Funcs, Arrays, Loops/01 Grades.c
+++ 03 C Basics/03 Funcs, Arrays, Loops/01 Grades.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+// Functional Prototyping
+// define the header of the function
+// stating types for parameters and return
+double computeAverage(double values[]);
+
 int main() {
     // An array of grades
     // In C, the [] goes after the name of the variable instead of the type
@@ -10,6 +15,19 @@ int main() {
     // Pointers are memory location
     // Identifier for the array accesses its pointer
     printf("grades memory location: %p\n", grades);
+
+    // arrays decay to pointers when you pass them into functions
+    // calculate the size on the outside of the function
+    int length = sizeof(grades) / sizeof(double);
+    double average = computeAverage(grades);
     
     return 0;
+}
+
+double computeAverage(double values[]) {
+    double sum = 0.0;
+    int length = sizeof(values) / sizeof(double);
+    for (int i = 0; i < length; i++) {
+        
+    }
 }
