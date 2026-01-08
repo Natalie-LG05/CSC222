72fec5c341539b05651115480dbafe960a0e2e45
Natalie Gates <natalielgates05@gmail.com>
2026-01-08 15:34:36 -0600

    changes

--- Lab3/question1.c
+++ Lab3/question1.c
@@ -7,8 +7,8 @@ int main(char *argv[], int argc) {
         printf("Must provide at most 6 arguments.\n");
     } else {
         for (int i = 0; i < argc; i++) {
-            if (i % 2 == 1) printf("%c ", argv[i][0])  // print the 1st character of odd arguments
-            else printf("%c ", argv[i][1])  // print the 2nd character of even arguments
+            if (i % 2 == 1) printf("%c ", argv[i][0]);  // print the 1st character of odd arguments
+            else printf("%c ", argv[i][1]);  // print the 2nd character of even arguments
         }
         printf("\n");  // add a newline to the end of the line printed by the preceding for loop
     }
