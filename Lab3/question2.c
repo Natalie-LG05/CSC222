4ae4be074b709c60b1a0dffb25c2f9e4451d227c
Natalie Gates <natalielgates05@gmail.com>
2026-01-08 15:58:04 -0600

    changes

--- Lab3/question2.c
+++ Lab3/question2.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 
+// constants
+const int HOURS_IN_DAY = 24;
+const int MIN_IN_HOUR = 60;
+const int SEC_IN_MIN = 60;
+
 int main() {
     printf("Enter a negative value at any point to quit.");
     while (1) {
-        int input;
+        int seconds;
         printf("Enter the number of seconds to convert: ");
-        scanf("%d", &input);
+        scanf("%d", &seconds);
 
-        if (input < 0) break;
+        if (seconds < 0) break;
 
-        printf("%d\n", input);
+        // integer division will give the number of whole days in the given number of seconds
+        int days = seconds / (SEC_IN_MIN * MIN_IN_HOUR * HOURS_IN_DAY);
+        seconds -= seconds % (SEC_IN_MIN * MIN_IN_HOUR * HOURS_IN_DAY);  // take off the number of seconds used to make up the whole days
+
+        int hours = seconds / (SEC_IN_MIN * MIN_IN_HOUR);  // get the number of whole hours left
+        seconds -= seconds % (SEC_IN_MIN * MIN_IN_HOUR); // take off the number of seconds used to make up the whole hours
+
+        int minutes = seconds / (SEC_IN_MIN);  // get the number of whole minutes
+        seconds -= seconds % (SEC_IN_MIN);  // take off the number of seconds used to make up the whole minutes
+
+        printf("%dd %dh %dm %ds", days, hours, minutes, seconds);
     }
     
     return 0;
