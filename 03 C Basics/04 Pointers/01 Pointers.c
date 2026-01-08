932b41e032df485de80f52b065fc2485a7e47740
Natalie Gates <natalielgates05@gmail.com>
2026-01-08 14:49:14 -0600

    changes

---
+++ 03 C Basics/04 Pointers/01 Pointers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // a pointer is a reference to a memory location
+    // use * when declaring them
+    // must use data types with pointers
+    // use & to get the memory address of some variable
+
+    // Example:
+    int num = 10;
+    int *numPtr = &num;  // can read as "address of num"
+
+    printf("Variable num: %d\n", num);
+    printf("Variable numPtr: %p\n", numPtr);
+
+    // You can use * to dereference a pointer
+    // that is, you can get the value at that memory location instead of the memory location
+    // The * here is called the indirection operator
+    printf("Variable num: %d\n", *numPtr);
+    
+    return 0;
+}
