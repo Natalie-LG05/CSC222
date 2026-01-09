70cfba91ad1d0bce32156bdcbcf0c022d4c4cbe2
Natalie Gates <natalielgates05@gmail.com>
2026-01-08 21:52:45 -0600

    changes

---
+++ Homeworks/HW 3/riemann.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <math.h>
+
+// Functional Prototypes
+void printIntro();
+double promptUser(char var);
+void printDeltaStatement();
+double f(double x);
+double calculateRiemannSum(double a, double b, double delta);
+
+int main() {
+    printIntro();  // print the intro statement
+
+    // get all three user given variables
+    double a = promptUser('a');  // get a
+    double b = promptUser('b');  // get b
+    printDeltaStatement();  // print a statement about the accuracy of the calculation
+    double delta = promptUser('d');  // get delta
+
+    sum = calculateRiemannSum(a, b, delta);    
+    printf("The integral over the provided lmits is %.4lf", sum);
+    return 0;
+}
+
+void printIntro() {
+    printf("This program will calculate the integral of the function 3x^3 - 2x^2 between user defined limits: a and b\n");
+}
+
+double promptUser(char var) {
+    double result;
+    if (var == 'a') {
+        printf("What is the value of \"a\": ");
+        scanf("%lf", &result);
+    } else if (var == 'b') {
+        printf("What is the value of \"b\": ");
+        scanf("%lf", &result);
+    } else if (var == 'd') {
+        printf("What is the value of \"delta\": ");
+        scanf("%lf", &result);
+    }
+    return result;
+}
+
+void printDeltaStatement() {
+    printf("The accuracy of this calculation depends on the value of delta that you use.\n");
+}
+
+double f(double x) {
+    return (3*pow(x, 3)) - (2*pow(x, 2));
+}
+
+double calculateRiemannSum(double a, double b, double delta) {
+
+}
