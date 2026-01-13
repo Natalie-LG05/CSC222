077860baad00a035391e1520471bc66b8c1d3801
Natalie Gates <natalielgates05@gmail.com>
2026-01-13 14:32:58 -0600

    changes

--- 03 C Basics/05 Memory Allocation/01 Malloc and Calloc.c
+++ 03 C Basics/05 Memory Allocation/01 Malloc and Calloc.c
@@ -6,7 +6,7 @@ int main() {
 
     // prompt the user for info
     printf("how many items do you want in the array? ");
-    scanf("%d", &numItems);  // don't take integral input
+    scanf("%d", &numItems);  // don't take integral input, instead take it as a string then check it and cast it
 
     // an array of integers
     int *values = (int*) malloc(sizeof(int) * numItems);
@@ -15,6 +15,20 @@ int main() {
     // calloc handles the multiplication for you
     // note that calloc clears the allocated memory for you
     int *values2 = (int*) calloc(sizeof(int), numItems);
+
+    // print values
+    for (int i = 0; i < numItems; i++) {
+        printf("%d ", values[i]);
+    }
+    printf("\n");
+
+    // add some values to `values`
+    for (int i = 0; i < numItems; i++) {
+        int val;
+        printf("Give me half the value for the %dth item ", i);
+        scanf("%d", &val);
+        values[i] = val * 2;
+    }
 
     // print values
     for (int i = 0; i < numItems; i++) {
