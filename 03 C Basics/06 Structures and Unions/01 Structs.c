b59c22ccabafbb2b5fb33da64d3c50d57d5cbe3f
Natalie Gates <natalielgates05@gmail.com>
2026-01-13 15:16:15 -0600

    changes

--- 03 C Basics/06 Structures and Unions/01 Structs.c
+++ 03 C Basics/06 Structures and Unions/01 Structs.c
@@ -25,4 +25,5 @@ void printPerson(struct Person person) {
     printf("%s is located at %p\n", person.name, &person);
     printf("%s's age is located at %p\n", person.name, &person.age);
     printf("%s's name is located at %p\n", person.name, &person.name);
+    printf("The size of %s is %lu\n", person.name, sizeof(person));
 }
