6ffa7c104df178e45f62100a8a4b8aab3f7ea9d4
Natalie Gates <natalielgates05@gmail.com>
2026-01-13 16:49:14 -0600

    changes

--- Homeworks/HW 4/tokenizer.c
+++ Homeworks/HW 4/tokenizer.c
@@ -7,20 +7,26 @@ int main() {
         printf("$ ");
         fgets(input, 256, stdin);
 
+        // look through the user input token by token
         char *token = strtok(input, " ");
+        // if the first token is exit, then terminate
         if (strcmp(token, "exit\n") == 0) {
             break;
         }
 
-        for (int i = 0; i < 15; i++) {
+        printf("Line read: %s", input);
+        printf("Token(s):\n");
+
+        int numTokens = 0;
+        for (int i = 0; i < 256; i++) {
             if (token) {
-                printf("%s\n", token);
+                numTokens++;
+                printf(" %s\n", token);
                 token = strtok(NULL, " ");
             }
         }
-        
-        printf("Line read: %s", input);
-        printf("Token(s):\n");
+
+        printf("%d token(s) read\n\n");
     }
     
     return 0;
