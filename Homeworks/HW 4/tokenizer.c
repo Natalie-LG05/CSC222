2f5e6ca89458ef445b4a8d6c8f36e41eebe8ec05
Natalie Gates <natalielgates05@gmail.com>
2026-01-13 16:27:32 -0600

    changes

--- Homeworks/HW 4/tokenizer.c
+++ Homeworks/HW 4/tokenizer.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     while (1) {
         char input[256];
         printf("$ ");
         fgets(input, 256, stdin);
-        printf("%s", input);
+
+        if (strcmp(input, "exit")) {
+            break;
+        }
+        
+        printf("Line read: %s", input);
+        printf("Token(s):\n");
     }
     
     return 0;
