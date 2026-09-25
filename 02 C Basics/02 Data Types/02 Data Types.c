2db58ffee62440b134cbeb6753280ca00ab2b4cc
Natalie Gates <NatalieLGates05@gmail.com>
2026-09-25 08:25:54 -0500

    changes

---
+++ 02 C Basics/02 Data Types/02 Data Types.c
@@ -0,0 +1,17 @@
+#include <stdio.>
+#include <stdint.h>
+
+int main() {
+    // int
+    // an int is 4 bytes
+    // 4 bytes = 32 bits
+
+    int w;  // declare
+    int x = 10;  // declare and initialize
+    int32_t y = 5;
+
+    printf("The value of x is %d.\n", x);
+    printf("The size of an int is %lu.\n", sizeof(int));
+
+    return 0;
+}
